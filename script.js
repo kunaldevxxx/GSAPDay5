ca9f3c0b6c24b9ecb4329943cdb8aac2403a4268
Kunal Khare <khareji594@gmail.com>
2023-07-26 22:01:48 +0530

    Done Work

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-gsap.to("#page1 h1",{
+var t1=gsap.timeline()
+
+function time(){
+    var a=0
+    setInterval(function(){
+        a=a+Math.floor(Math.random()*15)
+        if(a<100){
+        document.querySelector("#loader h1").innerHTML=a+"%"
+        }
+        else{
+            a=100
+            document.querySelector("#loader h1").innerHTML=a+"%"
+        }
+    },150);
+
+}
+time()
+
+t1.to("#loader h1",{
+    scale:1.5,
+    delay:0.5,
+    duration:1,
+    onStart:time()
+})
+t1.to("#loader",{
+top:"-100%",
+delay:0.5,
+duration:2
+})
+t1.to("#page1 h1",{
     transform:"translateX(-100%)",
     fontWeight:"100",
     scrollTrigger:{
